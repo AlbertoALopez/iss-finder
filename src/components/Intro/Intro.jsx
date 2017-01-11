9aef5b8d2f7d6f05fe19ed8eab87b2ffd16796c9
Alberto Lopez <albertolopez19@gmail.com>
2017-01-10 21:24:55 -0500

    Update iss img to be responsive

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react';
+import './_Intro.scss';
 
 const Intro = () => (
   <Container text>
     <img
+      className="iss-img"
       src={require('./iss-large.png')}
     />
     <Header as="h2">Where in the world is the ISS?</Header>
