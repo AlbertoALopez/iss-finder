574c42f621ca0b69239e31cd2b15cd851246eb42
Alberto Lopez <albertolopez19@gmail.com>
2017-01-10 21:34:07 -0500

    Fix empty props by changing init map to willReceiveProps

--- src/components/LeafletMap/LeafletMap.jsx
+++ src/components/LeafletMap/LeafletMap.jsx
@@ -8,28 +8,29 @@ export default class LeafletMap extends Component {
     super(props);
   }
 
-  initMap() {
+  initMap(props) {
     const issIcon = L.icon({
       iconUrl: 'http://www.i2clipart.com/cliparts/c/b/7/7/clipart-international-space-station-cb77.png',
       iconSize: [50, 30],
       iconAnchor: [25, 15],
       popupAnchor: [50, 25],
     });
+    const { issLat, issLon } = props;
 
-    const map = L.map('map').setView([0,0], 2);
-    map.panTo(
-      [this.props.issLat, this.props.issLon]
-    );
+    const map = L.map('map').setView([issLat, issLon], 2);
+    // map.panTo(
+    //   [this.props.issLat, this.props.issLon]
+    // );
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-    L.marker([0, 0], {
+    L.marker([issLat, issLon], {
       icon: issIcon,
     }).addTo(map);
   }
 
-  componentDidMount() {
-    this.initMap();
+  componentWillReceiveProps(nextProps) {
+    this.initMap(nextProps);
   }
   render() {
     return (
